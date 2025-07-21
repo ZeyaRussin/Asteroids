2d2cc8bd23ebeffff62dccb536978c47fa62f2d2
ZeyaRussin <zeyarussin@gmail.com>
2025-07-20 23:50:02 -0400

    added multiple new classes and am almost done!

--- main.py
+++ main.py
@@ -1,17 +1,49 @@
 import pygame
 from constants import *
+from player import *
+from shot import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteriods = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatables, drawables)
+    Shot.containers = (shots, updatables, drawables)
+    Asteroid.containers = (asteriods, updatables, drawables)
+    AsteroidField.containers = (updatables)
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        
+        updatables.update(dt)
+        for a in asteriods:
+            if a.collision(player):
+                print("Game Over!")
+                return
+
+        screen.fill("black")
+
+        for i in drawables:
+            i.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
